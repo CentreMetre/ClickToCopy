d6cf0ca9477198f5d6ab1644db1a133ce15acdcb
Martin McLaren <luckyminer01@gmail.com>
2022-07-03 20:13:58 +0100

    V1.0 Build 1
    The app is functionally finished, but there is some thing I need to change such as having the app size change.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MenuItemStrip = new System.Windows.Forms.MenuStrip();
+            this.MenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditCharacterFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MenuItemStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuItemStrip
+            // 
+            this.MenuItemStrip.GripMargin = new System.Windows.Forms.Padding(0);
+            this.MenuItemStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.MenuItemStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFile});
+            this.MenuItemStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuItemStrip.Name = "MenuItemStrip";
+            this.MenuItemStrip.Padding = new System.Windows.Forms.Padding(0);
+            this.MenuItemStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.MenuItemStrip.Size = new System.Drawing.Size(120, 24);
+            this.MenuItemStrip.TabIndex = 3;
+            this.MenuItemStrip.Text = "menuStrip1";
+            this.MenuItemStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // MenuItemFile
+            // 
+            this.MenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EditCharacterFileToolStripMenuItem});
+            this.MenuItemFile.Name = "MenuItemFile";
+            this.MenuItemFile.Padding = new System.Windows.Forms.Padding(0);
+            this.MenuItemFile.Size = new System.Drawing.Size(29, 24);
+            this.MenuItemFile.Text = "File";
+            this.MenuItemFile.Click += new System.EventHandler(this.MenuItemFile_Click);
+            // 
+            // EditCharacterFileToolStripMenuItem
+            // 
+            this.EditCharacterFileToolStripMenuItem.Name = "EditCharacterFileToolStripMenuItem";
+            this.EditCharacterFileToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.EditCharacterFileToolStripMenuItem.Text = "Edit Character File...";
+            this.EditCharacterFileToolStripMenuItem.Click += new System.EventHandler(this.editCharacterFileToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reload App";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(120, 389);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MenuItemStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.MenuItemStrip;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ClickToCopy";
+            this.MenuItemStrip.ResumeLayout(false);
+            this.MenuItemStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private MenuStrip MenuItemStrip;
+        private ToolStripMenuItem MenuItemFile;
+        private ToolStripMenuItem EditCharacterFileToolStripMenuItem;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
 namespace ClickToCopy
 {
     public partial class Form1 : Form
     {
+        void CreateDirectory(string UserFolder,string AppData, string AppDataFolder, string FileCharList)
+        {
+            Directory.CreateDirectory(UserFolder + AppData + AppDataFolder);
+
+            if (UserFolder + AppData + AppDataFolder + FileCharList != null)
+            {
+
+                using (StreamWriter sw = new StreamWriter(UserFolder + AppData + AppDataFolder + FileCharList))
+                {
+                    sw.WriteLine
+(@"‽
+²
+³
+÷
+±
+≥
+≤");
+                }
+            }
+        }
+
+        string UserFolderFun()
+        {
+            string UserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return UserFolder;
+        }
+
+        string PathMidToEndFun()
+        {
+            string PathMidToEnd = @"\AppData\Local\ClickToCopy\Character List.txt";
+            return PathMidToEnd;
+        }
+
+        void GenerateButtons()
+        {
+            string UserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string PathMidToEnd = @"\AppData\Local\ClickToCopy\Character List.txt";
+            var LineCount = File.ReadLines(UserFolder + @"\AppData\Local\ClickToCopy\Character List.txt").Count(); //Gets the amount of lines in the file which is used to generate the correct amount of buttons
+            int LineCountInt = Convert.ToInt32(LineCount); //Converts the number of lines to a usable format (int)
+
+
+            int x = 39;
+            int y = 56;
+            using (StreamReader rs = new StreamReader(UserFolder + PathMidToEnd))
+            {
+                for (int i = 0; i < LineCountInt; i++)
+                {
+                    Button button = new Button(); //Creates new button object
+                    //button.Name = "Button1";
+                    button.Location = new Point(x, y);
+                    button.Size = new Size(30, 30);
+                    button.Text = rs.ReadLine();
+                    button.Font = new Font("Segoe UI", 12);
+                    button.Name = "NewButton";
+                    this.Controls.Add(button);
+                    button.Click += new EventHandler(button_Click);
+                    void button_Click(object sender, EventArgs e)
+                    {
+                        Clipboard.SetText(button.Text);
+                    }
+                    y = y + 36;
+                }
+            }
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            //A the paths and file names for the files the app will use.
+            string UserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); //Gets C:\Users\[USER]
+            string AppData = @"\AppData\Local"; //AppData folder
+            string AppDataFolder = @"\ClickToCopy"; //The folder in the Local folder 
+            string FileCharList = @"\Character List.txt"; //The character list file
+            string FileIni = @"\config.ini"; //The config file
+            string FullPathCharList = @"C:\Users\lucky\AppData\Local\ClickToCopy\Character List.txt";
+
+            if (!Directory.Exists(UserFolder + AppData + AppDataFolder))
+            {
+                CreateDirectory(UserFolder, AppData, AppDataFolder, FileCharList);
+            }
+
+            GenerateButtons(); 
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editCharacterFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", UserFolderFun() + PathMidToEndFun());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateButtons();
         }
     }
 }
